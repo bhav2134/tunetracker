1fb644bc4482cde54623e9791a3a1753c86b2079
Bhavdeep Arora <bhavdeepsa@gmail.com>
2024-07-19 11:55:11 -0400

    setup expressjs and react

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useEffect,useState } from 'react'
+
+function App() {
+
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
+
+  return (
+    <div>
+
+      {(typeof backendData.users === 'undefined') ? (
+        <p>Loading ...</p>
+      ): (
+        backendData.users.map((users,i) => (
+          <p key={i}>{users}</p>
+        ))
+      )}
+
+    </div>
+  )
+}
+
+export default App
